41200e8a83a878b24096e41c50d6586fc0056cf2
AlexandreFernas <121526733+AlexandreFernas@users.noreply.github.com>
2023-08-11 15:10:04 -0300

    Add files via upload

---
+++ base.js
@@ -0,0 +1,9 @@
+
+
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}0&limit=${limit}`
+fetch(url).then(function (response) {
+    console.log(response)
+})
+
